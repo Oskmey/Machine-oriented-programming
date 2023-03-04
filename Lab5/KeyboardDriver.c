8c43a6a647daefa2e9acfb79c6a6cfd78b8a8cf4
MarcoTaf <112405603+MarcoTaf@users.noreply.github.com>
2023-03-04 11:29:48 +0100

    Updates to KB

--- Lab5/KeyboardDriver.c
+++ Lab5/KeyboardDriver.c
@@ -1,10 +1,15 @@
 #include "KeyboardDriver.h"
 #include "GPIO.h"
 
-int kbdGetCol(void){
+
+
+KB_PORT KB_LOW = {&PORT_D.IDRLOW, &PORT_D.ODRLOW};
+KB_PORT KB_HIGH = {&PORT_D.IDRHIGH, &PORT_D.ODRHIGH};
+
+int kbdGetCol(KB_PORT* port){
 	int kbdGetCol(void){
 	unsigned char c; 
-	c = PORT_E.IDRHIGH; //rad värdet placeras i c 
+	c = *(port->IN); //rad värdet placeras i c 
 	if(c & 0x8) return 4;
 	if(c & 0x4) return 3;
 	if(c & 0x2) return 2;
@@ -14,31 +19,41 @@ int kbdGetCol(void){
 	}
 }
 
-void kdbActivate(unsigned int row) { //hjälp rutin (MULTIPLEX SAKER FATTAR EJ)
+void kdbActivate(unsigned int row, KB_PORT* port) { //hjälp rutin (MULTIPLEX SAKER FATTAR EJ)
 	switch(row){
-		case 1: PORT_E.ODRHIGH = 0x10; 
+		case 1: *(port->OUT) = 0x10; 
 			break; 
-		case 2: PORT_E.ODRHIGH = 0x20; 
+		case 2: *(port->OUT) = 0x20; 
 			break;
-		case 3: PORT_E.ODRHIGH = 0x40; 
+		case 3: *(port->OUT) = 0x40; 
 			break;
-		case 4: PORT_E.ODRHIGH = 0x80; 
+		case 4: *(port->OUT) = 0x80; 
 			break;
-		default: PORT_E.ODRHIGH = 0x0;
+		default: *(port->OUT) = 0x0;
 			break;
 	}
 }
 
-char keyb(void){
+char keyb(char target){
+	KB_PORT* target_port;
+	if (target)
+	{
+		target_port = &KB_HIGH;
+	}
+	else
+	{
+		target_port = &KB_LOW;
+	}
+	
 	char keys[] = {1,2,3,0xA,4,5,6,0xB,7,8,9,0x39,0xE,0,0xF6,0xD}; 
 	int row;
 	int col;
 	for(row = 1; row <= 4; row++){ //väljer en rad att se på
-		kdbActivate(row);			//Ger ström till den raden vi har valt 
-		if( col = kbdGetCol()){		
+		kdbActivate(row, target_port);			//Ger ström till den raden vi har valt 
+		if( col = kbdGetCol(target_port)){		
 			return ((4*row-1)+(col-1));
 		}
 	}
-	kdbActivate(0);
+	kdbActivate(0, target_port);
 	return 0xFF;
 }
--- Lab5/KeyboardDriver.h
+++ Lab5/KeyboardDriver.h
@@ -1,5 +1,10 @@
 #pragma once
 
-int kbdGetCol(void);
-void kdbActivate(unsigned int row);
-char keyb(void);
+typedef struct PORT{
+	unsigned char* IN;
+	unsigned char* OUT;
+}KB_PORT;
+
+int kbdGetCol(KB_PORT* port);
+void kdbActivate(unsigned int row, KB_PORT* port);
+char keyb(char target);
