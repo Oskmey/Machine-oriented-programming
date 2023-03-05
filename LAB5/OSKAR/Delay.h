0120c480cfe05fcc77a86ce9ac80319edfd53625
Oskar Meyer <112823957+Oskmey@users.noreply.github.com>
2023-03-05 19:31:21 +0100

    Delete LAB5/OSKAR directory

--- LAB5/OSKAR/Delay.h
+++
@@ -1,7 +0,0 @@
-
-
-#pragma once
-void delay_250ns( void );
-void delay_milli(unsigned int ms);
-void delay_micro(unsigned int us);
-void setup_ms_delay(void);
--- LAB5/OSKAR/Displaydriver.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-void graphic_initalize(void);
-void graphic_pixel_set(int x, int y);
-void graphic_clear_screen(void);
-void graphic_pixel_clear(int x, int y);
